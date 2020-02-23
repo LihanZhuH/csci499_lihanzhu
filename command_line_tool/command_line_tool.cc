de01e1da11d936d08886461355aada4e776468bb
Lihan Zhu <lihanzhu@usc.edu>
2020-02-23 12:15:05 -0800

    Finished command_line_tool

--- command_line_tool/command_line_tool.cc
+++ command_line_tool/command_line_tool.cc
@@ -1,8 +1,4 @@
-#include <string>
-#include <iostream>
-
-#include <gflags/gflags.h>
-#include <glog/logging.h>
+#include "command_line_tool.h"
 
 namespace command_line {
 
@@ -14,110 +10,235 @@ DEFINE_string(reply, "", "Indicates that the new warble is a reply to the given 
 DEFINE_string(follow, "", "Starts following the given username.");
 DEFINE_string(read, "", "Reads the warble thread starting at the given id.");
 DEFINE_bool(profile, false, "Gets the user’s profile of following and followers.");
+DEFINE_bool(hook, false, "Hooks all warble functions.");
 
-// Return true if flags are for REGISTER
-bool ValidRegCmd() {
+// -- Implementation of CommandLineHandler --
+
+bool CommandLineHandler::Init() {
+  // Hook all event functions
+  func_client_->Hook(kRegisteruserPair.first, kRegisteruserPair.second);
+  func_client_->Hook(kWarblePair.first, kWarblePair.second);
+  func_client_->Hook(kFollowPair.first, kFollowPair.second);
+  func_client_->Hook(kReadPair.first, kReadPair.second);
+  func_client_->Hook(kProfilePair.first, kProfilePair.second);
+}
+
+bool CommandLineHandler::Run() {
+  // Check each combination of arguments
+  if (ValidHookCmd()) {
+    return Init();
+  } else if (ValidRegCmd()) {
+    return SendRegisteruser();
+  } else if (ValidWarbleCmd()) {
+    return SendWarble();
+  } else if (ValidFollowCmd()) {
+    return SendFollow();
+  } else if (ValidReadCmd()) {
+    return SendRead();
+  } else if (ValidProfileCmd()) {
+    return SendProfile();
+  } else {
+    LOG(WARNING) << "CommandLine: Received INVALID OPTION";
+    return false;
+  }
+}
+
+bool CommandLineHandler::ValidRegCmd() {
   if (FLAGS_registeruser.empty()) {
     return false;
   }
   if (!FLAGS_user.empty() || !FLAGS_warble.empty() ||
       !FLAGS_reply.empty() || !FLAGS_follow.empty() ||
-      !FLAGS_read.empty() || FLAGS_profile) {
+      !FLAGS_read.empty() || FLAGS_profile || FLAGS_hook) {
     return false;
   }
   return true;
 }
 
-// Return true if flags are for NEW_WARBLE
-bool ValidWarbleCmd() {
+bool CommandLineHandler::ValidWarbleCmd() {
   if (FLAGS_user.empty() || FLAGS_warble.empty()) {
     return false;
   }
-  if (!FLAGS_registeruser.empty() || !FLAGS_reply.empty() ||
-      !FLAGS_follow.empty() || !FLAGS_read.empty() || FLAGS_profile) {
-    return false;
-  }
-  return true;
-}
-
-// Return true if flags are for NEW_REPLY
-bool ValidReplyCmd() {
-  if (FLAGS_user.empty() || FLAGS_warble.empty() ||
-      FLAGS_reply.empty()) {
-    return false;
-  }
   if (!FLAGS_registeruser.empty() || !FLAGS_follow.empty() ||
-      !FLAGS_read.empty() || FLAGS_profile) {
+      !FLAGS_read.empty() || FLAGS_profile || FLAGS_hook) {
     return false;
   }
   return true;
 }
 
-// Return true if flags are for FOLLOW
-bool ValidFollowCmd() {
+bool CommandLineHandler::ValidFollowCmd() {
   if (FLAGS_user.empty() || FLAGS_follow.empty()) {
     return false;
   }
-  if (!FLAGS_registeruser.empty() || !FLAGS_reply.empty() ||
+  if (!FLAGS_registeruser.empty() || !FLAGS_reply.empty() || FLAGS_hook ||
       !FLAGS_warble.empty() || !FLAGS_read.empty() || FLAGS_profile) {
     return false;
   }
   return true;
 }
 
-// Return true if flags are for READ_WARBLE
-bool ValidReadCmd() {
+bool CommandLineHandler::ValidReadCmd() {
   if (FLAGS_user.empty() || FLAGS_read.empty()) {
     return false;
   }
-  if (!FLAGS_registeruser.empty() || !FLAGS_reply.empty() ||
+  if (!FLAGS_registeruser.empty() || !FLAGS_reply.empty() || FLAGS_hook ||
       !FLAGS_warble.empty() || !FLAGS_follow.empty() || FLAGS_profile) {
     return false;
   }
   return true;
 }
 
-// Return true if flags are for READ_WARBLE
-bool ValidProfileCmd() {
+bool CommandLineHandler::ValidProfileCmd() {
   if (FLAGS_user.empty() || !FLAGS_profile) {
     return false;
   }
   if (!FLAGS_registeruser.empty() || !FLAGS_reply.empty() ||
       !FLAGS_warble.empty() || !FLAGS_follow.empty() ||
-      !FLAGS_read.empty()) {
+      !FLAGS_read.empty() || FLAGS_hook) {
     return false;
   }
   return true;
 }
 
-}  // command_line
-
-int main(int argc, char *argv[]) {
-  ::google::ParseCommandLineFlags(&argc, &argv, true);
-
-  // Check each combination of arguments
-  // TODO: Will call different function and send data to Func server
-  if (command_line::ValidRegCmd()) {
-    LOG(INFO) << "CommandLine: Received REGISTER";
-    return 0;
-  } else if (command_line::ValidWarbleCmd()) {
-    LOG(INFO) << "CommandLine: Received NEW_WARBLE";
-    return 0;
-  } else if (command_line::ValidReplyCmd()) {
-    LOG(INFO) << "CommandLine: Received NEW_REPLY";
-    return 0;
-  } else if (command_line::ValidFollowCmd()) {
-    LOG(INFO) << "CommandLine: Received FOLLOW";
-    return 0;
-  } else if (command_line::ValidReadCmd()) {
-    LOG(INFO) << "CommandLine: Received READ_WARBLE";
-    return 0;
-  } else if (command_line::ValidProfileCmd()) {
-    LOG(INFO) << "CommandLine: Received PROFILE";
-    return 0;
-  } else {
-    LOG(WARNING) << "CommandLine: Received INVALID OPTION";
-    return 1;
+bool CommandLineHandler::ValidHookCmd() {
+  if (!FLAGS_hook) {
+    return false;
   }
-  return 1;
+  if (!FLAGS_registeruser.empty() || !FLAGS_reply.empty() ||
+      !FLAGS_warble.empty() || !FLAGS_follow.empty() ||
+      !FLAGS_read.empty() || !FLAGS_user.empty() || FLAGS_profile) {
+    return false;
+  }
+  return true;
 }
+
+bool CommandLineHandler::SendRegisteruser() {
+  // Send request
+  warble::RegisteruserRequest request;
+  warble::RegisteruserReply reply;
+  google::protobuf::Any request_payload;
+  request.set_username(FLAGS_registeruser);
+  request_payload.PackFrom(request);
+  // Check reply
+  if (auto optional_payload =
+      func_client_->Event(kRegisteruserPair.first, request_payload)) {
+    optional_payload->UnpackTo(&reply);
+    std::cout << "Successfully registered!" << std::endl;
+    return true;
+  }
+  std::cout << "Failed to register." << std::endl;
+  LOG(WARNING) << "CommandLine: Register command failed";
+  return false;
+}
+
+bool CommandLineHandler::SendWarble() {
+  // Send request
+  warble::WarbleRequest request;
+  warble::WarbleReply reply;
+  google::protobuf::Any request_payload;
+  request.set_username(FLAGS_user);
+  request.set_text(FLAGS_warble);
+  if (!FLAGS_reply.empty()) {
+    request.set_parent_id(FLAGS_reply);
+  }
+  request_payload.PackFrom(request);
+  // Check reply
+  if (auto optional_payload =
+      func_client_->Event(kWarblePair.first, request_payload)) {
+    optional_payload->UnpackTo(&reply);
+    std::cout << "Successfully posted warble!" << std::endl;
+    return true;
+  }
+  std::cout << "Failed to post warble." << std::endl;
+  LOG(WARNING) << "CommandLine: Warble command failed";
+  return false;
+}
+
+bool CommandLineHandler::SendFollow() {
+  // Send request
+  warble::FollowRequest request;
+  warble::FollowReply reply;
+  google::protobuf::Any request_payload;
+  request.set_username(FLAGS_user);
+  request.set_to_follow(FLAGS_follow);
+  request_payload.PackFrom(request);
+  // Check reply
+  if (auto optional_payload =
+      func_client_->Event(kFollowPair.first, request_payload)) {
+    optional_payload->UnpackTo(&reply);
+    std::cout << "Successfully followed!" << std::endl;
+    return true;
+  }
+  std::cout << "Failed to follow." << std::endl;
+  LOG(WARNING) << "CommandLine: Follow command failed";
+  return false;
+}
+
+bool CommandLineHandler::SendRead() {
+  // Send request
+  warble::ReadRequest request;
+  warble::ReadReply reply;
+  google::protobuf::Any request_payload;
+  request.set_warble_id(FLAGS_read);
+  request_payload.PackFrom(request);
+  // Check reply
+  if (auto optional_payload =
+      func_client_->Event(kReadPair.first, request_payload)) {
+    optional_payload->UnpackTo(&reply);
+    // Display all warbles
+    auto repeated_warbles = reply.warbles();
+    for (int i = 0; i < repeated_warbles.size(); ++i) {
+      auto warble = repeated_warbles.at(i);
+      auto timestamp = warble.timestamp();
+      std::cout << "Username: " << warble.username() << std::endl
+                << "Time: " << timestamp.seconds() << std::endl
+                << "Warble ID: " << warble.id() << std::endl
+                << "Warble text: " << warble.text() << std::endl;
+      std::cout << std::endl;
+    }
+    return true;
+  }
+  std::cout << "Failed to read warble." << std::endl;
+  LOG(WARNING) << "CommandLine: Read command failed";
+  return false;
+}
+
+bool CommandLineHandler::SendProfile() {
+  // Send request
+  warble::ProfileRequest request;
+  warble::ProfileReply reply;
+  google::protobuf::Any request_payload;
+  request.set_username(FLAGS_user);
+  request_payload.PackFrom(request);
+  // Check reply
+  if (auto optional_payload =
+      func_client_->Event(kProfilePair.first, request_payload)) {
+    optional_payload->UnpackTo(&reply);
+    // Display followers
+    if (reply.followers_size() == 0) {
+      std::cout << "Followers: none" << std::endl;
+    } else {
+      auto repeated_followers = reply.followers();
+      std::cout << "Followers:" << std::endl;
+      for (int i = 0; i < repeated_followers.size(); ++i) {
+        std::cout << '\t' << repeated_followers.at(i) << std::endl;
+      }
+    }
+    // Display followings
+    if (reply.following_size() == 0) {
+      std::cout << "Followings: none" << std::endl;
+    } else {
+      auto repeated_followings = reply.following();
+      std::cout << "Followings:" << std::endl;
+      for (int i = 0; i < repeated_followings.size(); ++i) {
+        std::cout << '\t' << repeated_followings.at(i) << std::endl;
+      }
+    }   
+    return true;
+  }
+  std::cout << "Failed to display profile." << std::endl;
+  LOG(WARNING) << "CommandLine: Profile command failed";
+  return false;
+}
+
+}  // command_line
--- command_line_tool/command_line_tool.h
+++ command_line_tool/command_line_tool.h
@@ -1,12 +1,93 @@
 #ifndef COMMAND_LINE_TOOL_H_
 #define COMMAND_LINE_TOOL_H_
 
+#include <memory>
+#include <string>
+#include <iostream>
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <google/protobuf/any.pb.h>
+
+#include "func/func_client.h"
+#include "warble/warble.pb.h"
+
 namespace command_line {
 
-class CommandParser {
+// Handler class for command line interface
+class CommandLineHandler {
+ public:
+  // Constructor that takes in a func_client pointer
+  CommandLineHandler(std::shared_ptr<func::FuncClientAbstract> client)
+      : func_client_(client) {}
 
+  // Disable move and copy
+  CommandLineHandler(const CommandLineHandler&) = delete;
+  CommandLineHandler& operator=(const CommandLineHandler&) = delete;
+
+  // Initialization: hook all event functions
+  bool Init();
+
+  // Parse command line flag and request for message
+  bool Run();
+
+ private:
+  // Define a pair with event_type and event_function name
+  typedef std::pair<int, std::string> event_pair;
+
+  // -- Flag validation helper functions --
+
+  // Return true if flags are for REGISTER
+  bool ValidRegCmd();
+  
+  // Return true if flags are for NEW_WARBLE
+  bool ValidWarbleCmd();
+
+  // Return true if flags are for FOLLOW
+  bool ValidFollowCmd();
+
+  // Return true if flags are for READ_WARBLE
+  bool ValidReadCmd();
+
+  // Return true if flags are for READ_WARBLE
+  bool ValidProfileCmd();
+
+  // Return true if flags are for hook
+  bool ValidHookCmd();
+
+  // -- Communication helper functions --
+
+  // Send registeruser request to Func server through func_client
+  // Success/failure signaled by return value
+  bool SendRegisteruser();
+
+  // Send registeruser request to Func server through func_client
+  // Success/failure signaled by return value
+  bool SendWarble();
+
+  // Send registeruser request to Func server through func_client
+  // Success/failure signaled by return value
+  bool SendFollow();
+
+  // Send registeruser request to Func server through func_client
+  // Success/failure signaled by return value
+  bool SendRead();
+
+  // Send registeruser request to Func server through func_client
+  // Success/failure signaled by return value
+  bool SendProfile();
+
+  // Pointer to Func client that communicates to Func server
+  std::shared_ptr<func::FuncClientAbstract> func_client_;
+
+  // Event pairs
+  const event_pair kRegisteruserPair = event_pair(1, "registeruser");
+  const event_pair kWarblePair = event_pair(2, "warble");
+  const event_pair kFollowPair = event_pair(3, "follow");
+  const event_pair kReadPair = event_pair(4, "read");
+  const event_pair kProfilePair = event_pair(5, "profile");
 };
 
 }  // command_line
 
-# endif  // COMMAND_LINE_TOOL_H_
+#endif  // COMMAND_LINE_TOOL_H_
